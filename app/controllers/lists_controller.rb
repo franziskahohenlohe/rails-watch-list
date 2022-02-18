0094096b81f5cd8ffa4edace6692697eadbb24bc
Franziska Hohenlohe <f.hohenlohe@hotmail.com>
2022-02-18 17:12:43 +0100

    fien

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,6 +5,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
@@ -12,13 +13,13 @@ class ListsController < ApplicationController
   end
 
   def create
-  @list = List.new(list_params)
+    @list = List.new(list_params)
   if @list.save
-  redirect_to @list, notice: 'A list was created'
+    redirect_to @list, notice: 'A list was created'
   else
     render :new
   end
-end
+  end
 
 private
   def list_params
