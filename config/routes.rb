174e083ff50f903e9b3cfdcdcfd961024ccdf310
Franziska Hohenlohe <f.hohenlohe@hotmail.com>
2022-02-18 11:14:55 +0100

    Ich habe keine Ahnung was ich mache

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :index, :new, :show, :create ] do
+    resources :bookmarks, only: [ :new, :create]
+    #GET "lists/42/bookmarks/new"
+    #POST "lists/42/bookmarks"
+  end
+  resources :bookmarks, only: :destroy
+  #DELETE "bookmarks/:id"
 end
